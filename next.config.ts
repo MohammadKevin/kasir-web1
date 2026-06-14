85d1fa8af26b484ff783c80f07531e93bcc987f3
MohammadKevin <kvn4.200581@gmail.com>
2026-06-14 12:01:53 +0700

    chore: optimize next.config.ts and remove deprecated eslint key

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    workerThreads: false,
+    cpus: 1
+  }
 };
 
 export default nextConfig;
